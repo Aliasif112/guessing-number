eb88a01c7a4d27e291f817375971f38d7567d867
Aliasif112 <148280839+Aliasif112@users.noreply.github.com>
2024-04-08 06:27:55 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import { log } from "console";
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "enter your guess number(Number limit from 1 to 5):",
+    }
+]);
+ 
+if (answer.userguessednumber === randomNumber){
+ console.log("congratulation ! your guess a correct number.");
+}
+else{
+    console.log("sorry , you guess a wrong number");
+}
